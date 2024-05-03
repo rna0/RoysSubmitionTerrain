73ca55e5ebdcc9bf3e23d95b5e8014035dc10bc0
RNA <rna0@protonmail.com>
2024-05-03 16:12:04 +0300

    made a mirror and cleaned up a bit

---
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Camera playerCamera;
+    private float _xRotation;
+
+    public float xSensitivity = 80f;
+    public float ySensitivity = 80f;
+
+    public void ProcessLook(Vector2 input)
+    {
+        var mouseX = input.x;
+        var mouseY = input.y;
+
+        _xRotation -= mouseY * Time.deltaTime * ySensitivity;
+        _xRotation = Mathf.Clamp(_xRotation, -80f, 80f);
+
+        playerCamera.transform.localRotation = Quaternion.Euler(_xRotation, 0, 0);
+        transform.Rotate(Vector3.up * mouseX * Time.deltaTime * xSensitivity);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool isGrounded;
+    public float playerSpeed = 7f;
+    public float gravity = -9.81f;
+
+    public float jumpHeight = 0.75f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+    }
+
+    public void ProcessMovement(Vector2 input)
+    {
+        Vector3 movementDirection = Vector3.zero;
+        movementDirection.x = input.x;
+        movementDirection.z = input.y;
+        controller.Move(transform.TransformDirection(movementDirection) * playerSpeed * Time.deltaTime);
+        playerVelocity.y += gravity * Time.deltaTime;
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+
+        controller.Move(playerVelocity * Time.deltaTime);
+        Debug.Log(playerVelocity.y);
+    }
+
+    public void jump()
+    {
+        if (isGrounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3f * gravity);
+        }
+    }
+}
